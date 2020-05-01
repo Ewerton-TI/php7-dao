61e251c88e465b1ae005b2e9afcd6c7a0790be53
Ewerton Oliveira <ewertonoliveiramartins@hotmail.com>
2020-04-30 21:52:34 -0300

    Iniciamos o Projeto

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        require_once ("config.php");
+        $sql = new sql();
+        
+        $usuarios = $sql->select("SELECT *FROM tb_usuarios");
+        
+        echo json_encode($usuarios);
+        ?>
+    </body>
+</html>
